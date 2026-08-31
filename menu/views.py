1b6142d83f6b41e3c116ca5586669e35cb42c8eb
Shayan Shafighi Shahri <128987822+ShayanSh96@users.noreply.github.com>
2026-08-31 13:17:37 +0330

    Add menu list and detail views

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Category, MenuItem
+
+
+def menu_home(request):
+    categories = Category.objects.filter(is_active=True).prefetch_related('items')
+    return render(request, 'menu/home.html', {'categories': categories})
+
+
+def item_detail(request, slug):
+    item = get_object_or_404(MenuItem.objects.select_related('category'), slug=slug)
+    return render(request, 'menu/item_detail.html', {'item': item})
