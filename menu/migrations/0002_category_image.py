b236004443f6086b68f044304e8c8f76f46fc3a8
Shayan Shafighi Shahri <128987822+ShayanSh96@users.noreply.github.com>
2026-08-31 13:27:44 +0330

    Add category image migration

---
+++ menu/migrations/0002_category_image.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("menu", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="category",
+            name="image",
+            field=models.ImageField(
+                blank=True,
+                null=True,
+                upload_to="menu/categories/",
+                verbose_name="تصویر دسته‌بندی",
+            ),
+        ),
+    ]
