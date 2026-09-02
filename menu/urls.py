dd17dbec72dab392300586e6c2f41ad04d91ef4f
Shayan Shafighi Shahri <128987822+ShayanSh96@users.noreply.github.com>
2026-09-02 11:37:28 +0330

    Route story and family pages

--- menu/urls.py
+++ menu/urls.py
@@ -6,6 +6,8 @@ app_name = 'menu'
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('story/', views.story, name='story'),
+    path('family/', views.family, name='family'),
     path('menu/', views.menu_home, name='home'),
     path('menu/<slug:slug>/', views.item_detail, name='item_detail'),
 ]
